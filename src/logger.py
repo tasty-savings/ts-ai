1c667175162029f733653ef596feb150802e0260
minji0916 <a2927036@naver.com>
2024-12-21 13:53:39 +0900

    Refactor : grapana 사용위해 logger 표준출력으로 변경

--- src/logger.py
+++ src/logger.py
@@ -2,20 +2,32 @@ import logging
 
 # 공통 핸들러와 포맷터 설정
 file_handler = logging.FileHandler('main_debug.log', mode='a')
+stream_handler = logging.StreamHandler()  # 터미널 출력용 핸들러
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
 
 # recipe_logger 설정
 logger_recipe = logging.getLogger('recipe_logger')
 logger_recipe.setLevel(logging.DEBUG)
 logger_recipe.addHandler(file_handler)
+logger_recipe.addHandler(stream_handler)  # 터미널 출력 추가
 
 # main_logger 설정
 logger_main = logging.getLogger('main_logger')
 logger_main.setLevel(logging.DEBUG)
 logger_main.addHandler(file_handler)
+logger_main.addHandler(stream_handler)  # 터미널 출력 추가
 
 # db_logger 설정
 logger_db = logging.getLogger('db_logger')
 logger_db.setLevel(logging.DEBUG)
 logger_db.addHandler(file_handler)
+logger_db.addHandler(stream_handler)  # 터미널 출력 추가
+
+# eval_logger 설정
+logger_eval = logging.getLogger('eval_logger')
+logger_eval.setLevel(logging.DEBUG)
+logger_eval.addHandler(file_handler)
+logger_eval.addHandler(stream_handler)  # 터미널 출력 추가
